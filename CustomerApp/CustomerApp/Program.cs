81a09536ce24c08cfb19b0581005ff7df1521a26
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-10-06 12:54:41 +0530

    updated

--- CustomerApp/CustomerApp/Program.cs
+++ CustomerApp/CustomerApp/Program.cs
@@ -2,3 +2,33 @@
 //Console.WriteLine("Hello, World!");
 
 
+//create objects of the Customer class
+using CustomerApp.Models;
+
+Customer customer = new Customer
+{
+    CustomerId = Faker.RandomNumber.Next(100,10000),
+    Name = new FullName { FirstName = Faker.Name.First(), LastName =Faker.Name.Last() },
+    Address = new Address
+    {
+        DoorNo = Faker.Company.BS(),
+        Street = Faker.Address.StreetSuffix(),
+        City = Faker.Address.CitySuffix(),
+        State = Faker.Address.CitySuffix(),
+        ZipCode = Faker.Address.ZipCode()
+    },
+    Email = Faker.Internet.Email(),
+    Password = Faker.Identification.UsPassportNumber(),
+    PhoneNumber = Faker.Phone.Number()
+
+};
+
+Console.WriteLine($"Customer ID: {customer.CustomerId}");   
+Console.WriteLine($"Customer Name: {customer.Name.FirstName} {customer.Name.LastName}");
+Console.WriteLine($"Customer Address: {customer.Address.DoorNo}, {customer.Address.Street}, {customer.Address.City}, {customer.Address.State} - {customer.Address.ZipCode}");
+Console.WriteLine($"Customer Email: {customer.Email}");
+Console.WriteLine($"Customer Phone Number: {customer.PhoneNumber}");
+Console.WriteLine($"Customer Password: {customer.Password}");
+Console.WriteLine("--------------------------------------------------");
+
+Console.ReadKey();
