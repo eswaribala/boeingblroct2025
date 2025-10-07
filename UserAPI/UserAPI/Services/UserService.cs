e46a103d57d5f3d0593e552f3476a62d0364f8f3
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-10-07 14:15:15 +0530

    updated

---
+++ UserAPI/UserAPI/Services/UserService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserAPI.Contexts;
+using UserAPI.Models;
+
+namespace UserAPI.Services
+{
+    public class UserService : IUserService
+    {
+        private UserContext _userContext;
+
+        public UserService(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+        public async Task<BoeingUser> AddUser(BoeingUser newUser)
+        {
+           var result= await _userContext.Users.AddAsync(newUser);
+            await _userContext.SaveChangesAsync();
+            return result.Entity;
+
+        }
+
+        public Task<bool> DeleteUser(long id)
+        {
+            var user = _userContext.Users.FirstOrDefaultAsync(u => u.UserId == id);
+            if (user != null)
+            {
+                _userContext.Users.Remove(user.Result);
+                _userContext.SaveChangesAsync();
+                return Task.FromResult(true);
+            }
+            return Task.FromResult(false);
+        }
+
+        public async Task<List<BoeingUser>> GetAllUsers()
+        {
+           return await _userContext.Users.ToListAsync();
+        }
+
+        public async Task<BoeingUser> GetUserById(long id)
+        {
+            var user= await _userContext.Users.FirstOrDefaultAsync(u => u.UserId == id);
+            if(user!=null)
+            {
+                return user;
+            }
+            return null;
+        }
+
+        public async Task<BoeingUser> UpdateUser(BoeingUser updatedUser)
+        {
+            var user = _userContext.Users.FirstOrDefaultAsync(u => u.UserId == updatedUser.UserId);
+            if (user != null)
+            {
+                _userContext.Entry(user).CurrentValues.SetValues(updatedUser);
+                await _userContext.SaveChangesAsync();
+                return updatedUser;
+            }
+            return null;
+        }
+    }
+}
