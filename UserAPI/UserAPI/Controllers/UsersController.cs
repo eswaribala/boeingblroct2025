e55b2b543af25b99cb1ed695a0ff8ac39be1052c
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-10-07 15:47:32 +0530

    updated

--- UserAPI/UserAPI/Controllers/UsersController.cs
+++ UserAPI/UserAPI/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UserAPI.Services;
 using AutoMapper;
+using UserAPI.DTOS;
+using UserAPI.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,27 +24,36 @@ namespace UserAPI.Controllers
 
         // GET: api/<UsersController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public Task<IEnumerable<IActionResult>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var users =  _userService.GetAllUsers();
+            var userReadDTOs = _mapper.Map<IEnumerable<BoeingUserReadDTO>>(users);
+            return Task.FromResult(userReadDTOs.Select(dto => (IActionResult)Ok(dto)));
+
         }
 
         // GET api/<UsersController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> Get(long id)
         {
-            return "value";
+           var user = await  _userService.GetUserById(id);
+           return Ok(user);
         }
 
         // POST api/<UsersController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] BoeingUserDTO boeingUserDTO)
         {
+            var boeingUser = _mapper.Map<BoeingUser>(boeingUserDTO);
+            var createdUser = await _userService.AddUser(boeingUser);
+            var boeingUserReadDTO = _mapper.Map<BoeingUserReadDTO>(createdUser);
+            return Ok(boeingUserReadDTO);
+            
         }
 
         // PUT api/<UsersController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] )
         {
         }
 
