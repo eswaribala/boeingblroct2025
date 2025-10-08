70a246f8d702800aa887b869280bf031a96b04cf
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-10-08 15:04:17 +0530

    updated

---
+++ PolicyHolderFunction/PolicyHolderFunction/Data/IPolicyHolderRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolicyHolderFunction.Data
+{
+    public interface IPolicyHolderRepository
+    {
+        Task<bool> AddPolicyHolderAsync(Models.PolicyHolder policyHolder);
+        Task<Models.PolicyHolder> GetPolicyHolderAsync(string policyNo);
+        Task<IEnumerable<Models.PolicyHolder>> GetAllPolicyHoldersAsync();
+        Task<bool> UpdatePolicyHolderAsync(Models.PolicyHolder policyHolder);
+        Task<bool> DeletePolicyHolderAsync(string policyNo);
+    }
+}
