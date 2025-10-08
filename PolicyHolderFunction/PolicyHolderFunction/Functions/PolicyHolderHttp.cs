518b92cbbdc13739fb1db570f613a7391ee737e7
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-10-08 16:00:10 +0530

    updated

---
+++ PolicyHolderFunction/PolicyHolderFunction/Functions/PolicyHolderHttp.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolicyHolderFunction.Functions
+{
+    public class PolicyHolderHttp
+    {
+
+        private readonly ILogger<PolicyHolderHttp> _logger;
+
+        public PolicyHolderHttp(ILogger<PolicyHolderHttp> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("Create")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
